a877e90464f9ed023151be6242f877541b7a5200
Patrice De Saint Steban <patou@users.noreply.github.com>
2010-06-14 12:52:48 +0000

    Amélioration graphique des notifications lives

--- directLive.js
+++ directLive.js
@@ -1,19 +1,19 @@
-			function displayDirect(item, displayDateTime) {
+			function displayDirect(item, popupDisplay) {
 				html = '<div><div class="liveHead '+item['sport']+'">Live '+item['sport']+': '+item['competition']+' '+item['journee'];
-				if (displayDateTime) {
+				if (popupDisplay) {
 					html += '<br/> '+ displayDateTime(item);
 				}
 				html += '</div>';
 				html += '<div id="match" class="score"><div class="board">';
 				switch(item['sport']) {
 					case "FOOT":									
-						html += loadDirectFoot(item); 	         
+						html += loadDirectFoot(item, popupDisplay); 	         
 						break;
 					case "TENNIS":									
-						html += loadDirectTennis(item);             
+						html += loadDirectTennis(item, popupDisplay);             
 						break;
 					case "BASKET":									
-						html += loadDirectBasket(item);           
+						html += loadDirectBasket(item, popupDisplay);           
 						break;
 					default: 
 						html += '<div class="scx"> ';
@@ -61,7 +61,14 @@
 			}
 			
 			
-			function loadDirectFoot(item) {
+			function loadDirectFoot(item, popupDisplay) {
+			
+				if (!popupDisplay) {
+					html += '<div class="statut">';
+					html += item['temps'] + " - ";
+					html += afficheStatut (item['statut']);
+					html += '</div>'; 
+				}
 				 html =  '<div class="logo1"> <a href="http://www.lequipe.fr/Football/FootballFicheClub'+item['id1']+'.html" target="_blank" class="eq1">';
 				html += '<img src="http://img.lequipe.fr/Football/logos/FootballLogo'+item['id1']+'_s60.gif" />';
 				html += '<br/>'+item['equipe_1']+'</a></div>';
@@ -77,12 +84,11 @@
 				{
 					html += '<div class="sc1">'+score[0]+'</div>';
 				}
-				
-				html += '<div class="statut">';
-				
+				if (popupDisplay) {
+					html += '<div class="statut">';
 					html += afficheStatut (item['statut']);
-				
-				html += '</div>'; 
+					html += '</div>'; 
+				}
 				if (score != null)
 				{
 					html += '<div class="sc2">'+score[1]+'</div>';
@@ -106,7 +112,7 @@
 			 * @param {Object} item
 			 */
 			
-			function loadDirectBasket(item) {
+			function loadDirectBasket(item, popupDisplay) {
 				 html =  '<div class="logo1"> ';
 				html += '<img src="http://www.lequipe.fr/Basket/logos/BasketLogo'+item['id1']+'_s60.gif" />';
 				html += '<br/>'+item['equipe_1']+'</div>';
@@ -124,9 +130,9 @@
 				}
 				
 				html += '<div class="statut">';
-				
+				if (!popupDisplay) {
 					html += afficheStatut (item['statut']);
-				
+				}
 				html += '</div>'; 
 				if (score != null)
 				{
@@ -143,7 +149,7 @@
 			}
 			
 			
-			function loadDirectTennis(item) {
+			function loadDirectTennis(item, popupDisplay) {
 				
 				
 				html =  '<div class="logo1"> <a href="http://www.lequipe.fr/Tennis/TFJM_'+item['id1']+'.html" target="_blank" class="eq1">';
@@ -164,7 +170,7 @@
 						html += '<table class="tennis_table">';
 						for (var i = 0; i < score.length; i++){
 							score2 = score[i].split('-');
-							
+							if (score2 && score2.length > 1)
 							html += '<tr><td>'+score2[0]+'</td><td>'+score2[1]+'</td></tr>';
 						};
 						html += '</table>';
